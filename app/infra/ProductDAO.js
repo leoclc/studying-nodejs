f460501def90c5d096266182fe375527f760213e
Leonardo <leoclc@hotmail.com>
2017-08-30 23:22:00 +0100

    fix

--- app/infra/ProductDAO.js
+++ app/infra/ProductDAO.js
@@ -2,14 +2,10 @@ function ProductDAO(connection){
 	this._connection = connection;
 }
 
-ProductDAO.prototype.lista = function(callback){
+ProductDAO.prototype.list = function(callback){
 	this._connection.query("select * from produtos", callback);
 }
 
-ProductDAO.prototype.remove = function(callback){
-	this._connection.query("delete from produtos where", callback);
-}
-
 module.exports = function (){
-	return new ProductDAO;
+	return ProductDAO;
 }
